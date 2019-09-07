5b44a3ea2d3b9f88815612712840616df73b0203
Ahmet Altay <aaltay@gmail.com>
2019-09-06 18:59:57 -0700

    Relax upper bound for futures dependency
    This would make it easier for its users to use latest version of futures.

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 install_requires = [
-    'futures==3.2.0',
+    'futures>=3.2.0,<4.0.0',
 ] if sys.version_info <= (3, 2) else []
 
 setup(
